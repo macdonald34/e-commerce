f06908053f96f9785ee556058d7e7aba49ed1877
macdonald34 <macdonaldmuhavi98@gmail.com>
2025-04-23 13:11:25 +0300

    done

---
+++ ecommerce/src/App.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        
+        
+      
+      </Routes>
+    </Router>
+  );
+}
---
+++ ecommerce/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import react from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import 'Navbar.css'
+
+const Navbar = () => {
+  
+
+  return (
+    <>
+    <div className='header'>
+        </div></>
+  )
+}
+
+export default Navbar;
